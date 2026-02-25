afcdf0221470b3a4f38765b3e4d6a5bccd19e1e1
Jessica Rodriguez <jessy512dr@gmail.com>
2026-02-24 21:21:50 -0600

    Actualizacion 20

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,6 @@ const { Pool } = require('pg');
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const path = require("path");
-app.use(express.static(path.join(__dirname, '../public')));
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 require("dotenv").config();
@@ -33,6 +32,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../public')));
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 
